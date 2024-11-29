ed48d2dad72fb4ca8a2d72aaefab33ce3286dcb3
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 17:37:56 +0500

    kjhg

---
+++ while1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace whilecw
+{
+    class Program
+    {
+        static void Main()
+        {
+            int b = 10;
+            int a = 50;
+            
+
+            while (b < a)
+            {
+                Console.WriteLine(b);
+                b++;
+               
+            }
+        }
+    }
+}
